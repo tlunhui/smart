26bd2222d4a23e7d5d5c47740b0cb10d4fc5a289
tlunhui <lun_hui@outlook.com>
2017-08-11 16:52:31 +0800

    smart

--- smartframework/src/main/java/org/smart4j/framework/util/CastUtil.java
+++ smartframework/src/main/java/org/smart4j/framework/util/CastUtil.java
@@ -1,28 +1,70 @@
 package org.smart4j.framework.util;
 
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.rmi.ServerException;
+
 public class CastUtil {
 
-    public static String CastString(Object obj, String defaultValue) {
-        return obj!=null?String.valueOf(obj):defaultValue;
+    public static String CastString(Object obj, String defaultValue) throws ServerException {
+        if (obj == null) {
+            return defaultValue;
+        }
+        if (obj instanceof String) {
+            return (String) obj;
+        }
+        throw new ServerException(obj + "不是一个字符串");
     }
 
-    public static String CastString(Object obj){
-        return CastString(obj,"");
+    public static String CastString(Object obj) throws ServerException {
+        return CastString(obj, "");
     }
 
-    public static int CastInt(Object obj,int defaultValue){
-        return 0;
+    public static int CastInt(Object obj, int defaultValue) throws ServerException {
+        return (int) CastDouble(obj);
     }
 
-    public static int CastInt(Object obj){
-        return CastInt(obj,0);
+    public static int CastInt(Object obj) throws ServerException {
+        return CastInt(obj, 0);
     }
 
-    public static boolean CastBoolean(Object obj,boolean defaultValue){
-        return true;
-    }
-    public static boolean CastBoolean(Object obj){
-        return CastBoolean(obj,true);
+    public static boolean CastBoolean(Object obj, boolean defaultValue) {
+        if (obj == null) {
+            return defaultValue;
+        }
+        if (!(obj instanceof String)) {
+            return defaultValue;
+        }
+        return BooleanUtils.toBoolean((String)obj);
     }
 
+    public static boolean CastBoolean(Object obj) {
+        return CastBoolean(obj, false);
+    }
+
+    public static double CastDouble(Object obj) throws ServerException {
+        return CastDouble(obj,0.0);
+    }
+
+    private static double CastDouble(Object obj, double defaultValue) throws ServerException {
+        if (obj == null) {
+            return defaultValue;
+        }
+        if (obj instanceof Number) {
+            return ((Number) obj).intValue();
+        }
+        if (obj instanceof String) {
+            String value = (String) obj;
+            if (value.length() < 1) {
+                return defaultValue;
+            }
+            if (!StringUtils.isNumeric(value)) {
+                return defaultValue;
+            }
+            return NumberUtils.toInt(value);
+        }
+        throw new ServerException(obj + "不是以个数字");
+    }
 }
--- smartframework/src/main/java/org/smart4j/framework/util/PropUtil.java
+++ smartframework/src/main/java/org/smart4j/framework/util/PropUtil.java
@@ -1,15 +1,49 @@
 package org.smart4j.framework.util;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropUtil {
 
-    public static Properties LoadFile(String filePath)
-    {
-        Properties props=null;
-        InputStream is=null;
+    public static Properties LoadFile(String filePath) throws FileNotFoundException {
+        Properties props = null;
+        InputStream is = null;
+        try {
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+            if (is == null) {
+                throw new FileNotFoundException(filePath + "file is not fount");
+            }
+            props = new Properties();
+            props.load(is);
+        } catch (IOException e) {
+
+        }
         return props;
     }
 
+    public static String GetString(Properties props, String key) throws Exception {
+        if (props != null && props.contains(key)) {
+           return props.getProperty(key);
+        }
+        throw  new Exception(key+"not found in properties");
+    }
+
+    public static int GetInt(Properties props,String key) throws Exception {
+        String stringValue=GetString(props,key);
+        return CastUtil.CastInt(stringValue);
+    }
+
+    public static double GetDouble(Properties props,String key) throws Exception {
+        String stringValue=GetString(props,key);
+        return CastUtil.CastDouble(stringValue);
+    }
+
+    public static boolean GetBoolean(Properties props,String key) throws Exception {
+        String stringValue=GetString(props,key);
+        return CastUtil.CastBoolean(stringValue);
+    }
 }
